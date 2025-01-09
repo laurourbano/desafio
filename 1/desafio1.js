a8c340d4e81001e634ef01c7aee8702535e29fbb
laurourbano <lauro@outlook.com.br>
2025-01-09 20:21:46 -0300

    layout: mudando pastas

---
+++ 1/desafio1.js
@@ -0,0 +1,8 @@
+var indice = 13;
+var soma = 0;
+var k = 0;
+while (k < indice) {
+    k++;
+    soma += k;
+}
+console.log(soma);
---
+++ 1/desafio1.ts
@@ -0,0 +1,11 @@
+let indice: number = 13;
+let soma: number = 0;
+
+let k: number = 0;
+
+while (k < indice) {
+    k++;
+    soma += k;
+}
+
+console.log(soma); // resultado: 91
